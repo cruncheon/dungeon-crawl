a24b5e84d59fab8a7da715dd7d4483b3783dd5b7
cruncheon <38970996+cruncheon@users.noreply.github.com>
2024-02-06 14:40:16 +0900

    Hallway button lights up hallway instead of dimming lights

--- scenes/entity/button_block2.gd
+++ scenes/entity/button_block2.gd
@@ -9,25 +9,31 @@ func interact():
 	if west_face == 7:
 		self.west_face = 8
 		self.bottom_face = -1
-		$"../../Lights/OmniLight2".light_color = Color(0.9, 0.4, 0.5)
+		
+		## Light Up Hallway
+		$"../../Lights/OmniLight2".light_color = Color("ffdabf")
 		$"../../Lights/OmniLight2".omni_range = 6
 		
-		$"../../Lights/OmniLight5".light_color = Color(0.9, 0.4, 0.5)
-		$"../../Lights/OmniLight5".omni_range = 3
+		$"../../Lights/OmniLight5".light_color = Color("ffdabf")
+		$"../../Lights/OmniLight5".omni_range = 6
 		
-		$"../../Lights/OmniLight6".light_color = Color(0.9, 0.4, 0.5)
-		$"../../Lights/OmniLight6".omni_range = 3
+		$"../../Lights/OmniLight6".light_color = Color("ffdabf")
+		$"../../Lights/OmniLight6".omni_range = 6
 		
 	elif west_face == 8:
 		self.west_face = 7
 		self.bottom_face = 5
-		$"../../Lights/OmniLight2".light_color = Color("#ffdabf")
-		$"../../Lights/OmniLight2".omni_range = 6
 		
-		$"../../Lights/OmniLight5".light_color = Color("#ffdabf")
-		$"../../Lights/OmniLight5".omni_range = 6
+		## Revert Hallway lights
+		$"../../Lights/OmniLight2".light_color = Color("ff4947")
+		$"../../Lights/OmniLight2".omni_range = 8
 		
-		$"../../Lights/OmniLight6".light_color = Color("#ffdabf")
-		$"../../Lights/OmniLight6".omni_range = 6
+		$"../../Lights/OmniLight5".light_color = Color("ff4947")
+		$"../../Lights/OmniLight5".omni_range = 3
+		
+		$"../../Lights/OmniLight6".light_color = Color("ff4947")
+		$"../../Lights/OmniLight6".omni_range = 3
+		
+		
 		
 
